536a1fd6244f93c9d8c662fa0b01b471c83af274
unknown <mohammadalirafat102@gmail.com>
2025-10-29 06:23:20 -0700

    connected backend with frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,10 +5,17 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 5000,
+    port: 5000,        // پورت React
     strictPort: true,
     hmr: {
       overlay: false
-    }
+    },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000', // آدرس Laravel
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 })
